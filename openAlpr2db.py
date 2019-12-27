9345cc601f592fd252316b1886f635c6076e0bdb
Saikat2019 <saikatmondal410@gmail.com>
2019-12-27 23:35:29 +0530

    commented and updated the README

--- openAlpr2db.py
+++ openAlpr2db.py
@@ -5,9 +5,9 @@ from pprint import pprint
 from datetime import datetime
 import os
 import sqlite3
-import base64
 import requests
 
+#connecting to beanstalkd 
 beanstalkc = beanstalkc.Connection(host='localhost',port=11300)
 TUBE_NAME='alprd'
 
@@ -26,12 +26,14 @@ pCoordinates = None
 pColor = None
 pVehicleType = None
 
+#path to local database, where the number plate informations will be stored
 DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'vehicles.db')
 
+# method to connect to our database
 def db_connect(db_path=DEFAULT_PATH):
     connection = sqlite3.connect(db_path)
     return connection
-
+# method to create table
 def create_table():
 	con = db_connect()
 	cur = con.cursor()
@@ -45,7 +47,7 @@ def create_table():
 		Image text NOT NULL,
 		TimeDate text NOT NULL)"""
 	cur.execute(vehicles_sql)
-
+# method to insert data
 def insert_data(number,Coordinates,Color,Vehicle,TimeDate):
 	con = db_connect()
 	cur = con.cursor()
@@ -55,7 +57,7 @@ def insert_data(number,Coordinates,Color,Vehicle,TimeDate):
 	con.commit()
 	cur.close()
 	con.close()
-
+# method to fetch all data
 def fetch_all():
 	con = db_connect()
 	cur = con.cursor()
@@ -65,7 +67,7 @@ def fetch_all():
 		print(r)
 	cur.close()
 	con.close()
-
+# method to upload a data to our database
 def upload_data():
 	con = db_connect()
 	cur = con.cursor()
@@ -84,15 +86,15 @@ create_table()
 
 
 while True:
-	job = beanstalkc.reserve()
-
+	job = beanstalkc.reserve() # it will be waiting infinitely, till a job
+								# is posted by the openalpr agent
 	if job is None:
 		# print("No number plate found")
 		pass
 	else:
 		# print("A number plate found")
 		pass
-		plates_info = json.loads(job.body)
+		plates_info = json.loads(job.body) # getting the detected plates info as a json
 
 		if 'data_type' not in plates_info:
 			#print('All openAlpr data should have data type. Some error occured!')
@@ -100,17 +102,17 @@ while True:
 		elif plates_info['data_type'] == 'alpr_results':
 			pass
 		elif plates_info['data_type'] == 'alpr_group':
-			pNumber = plates_info['best_plate_number']
-			pCoordinates = plates_info['best_plate']['coordinates']
-			pColor = plates_info['vehicle']['color'][0]['name']
-			pVehicleType = plates_info['vehicle']['body_type'][0]['name']
+			pNumber = plates_info['best_plate_number'] # plate number
+			pCoordinates = plates_info['best_plate']['coordinates'] #coordinate of plate in the frame
+			pColor = plates_info['vehicle']['color'][0]['name'] # vehicle color
+			pVehicleType = plates_info['vehicle']['body_type'][0]['name'] # vehicle type
 			timeDate = datetime.now().isoformat()
 			now = datetime.now()
 			img_name = now.strftime("%d-%m-%Y_%H:%M:%S")+'.jpg'
-			img_path = os.path.join(os.path.dirname(__file__),'Images',img_name)
+			img_path = os.path.join(os.path.dirname(__file__),'Images',img_name) # store the images of the vehicles
 
 			with open(img_path,'wb') as f:
-				f.write(base64.b64decode(plates_info['vehicle_crop_jpeg']))
+				f.write(base64.b64decode(plates_info['vehicle_crop_jpeg'])) # openalpr gives vehicle images as string, so converting to jpg
 				f.close()
 
 			# print(pNumber,'\n',pCoordinates,'\n',pColor,'\n',pVehicleType)
